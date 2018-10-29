fb14b2f7458803ce70ea855766b553b58326694a
SidGor <szlsd123@163.com>
2018-10-29 16:35:46 +0800

    Lazy version of trans code,with test data, contains two almost identical functions, welcome challenge

--- DataImport/TransCodeTSWD.py
+++ DataImport/TransCodeTSWD.py
@@ -12,6 +12,43 @@ handle cross system comparison.
 @author: szlsd
 """
 
-def TransCodeTSWD():
+import pandas as pd
+test_data_1 = "SH600021"
+test_data_2 = "600021.SZ"
+test_data_3 = ["SH600021", "SZ300621", "SH602391"]
+test_data_4 = ["600021.SH", "300621.SZ", "602391.SH"]
+test_data_5 = pd.Series(test_data_4)
+test_data_6 = {1:"600021.SH", 2:"300621.SZ", 3:"602391.SH"}
+
+def TransCodeT2W(tickers):
+    # Function that transform SHxxxxxx codes into xxxxxx.SH
+    TofD = type(tickers)
     
-    return
+    if TofD == str:
+        
+        converted = [tickers[2:8] + "." + tickers[0:2]]
+    else :
+        try:
+            converted = [(i[2:8] + "." + i[0:2]) for i in tickers]
+        except:
+            return "Str, list, pd.Series only, cannot convert current object!"
+        
+    return converted
+
+
+def TransCodeW2T(tickers):
+
+    # Function that transform xxxxxx.SH codes into SHxxxxxx    
+    TofD = type(tickers)
+    
+    if TofD == str:
+        
+        converted = [tickers[7:9] + tickers[0:6]]
+        
+    else :
+        try:
+            converted = [(i[7:9] + i[0:6]) for i in tickers]
+        except:
+            return "Str, list, pd.Series only, cannot convert current object!"
+        
+    return converted
