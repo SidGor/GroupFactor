d8daa60f62feca9f854baaf1d05df23692959cdc
SidGor <szlsd123@163.com>
2018-10-29 17:32:52 +0800

    initializing the TS data module, lots of things to figure out

--- DataImport/TSoperation.py
+++ DataImport/TSoperation.py
@@ -4,11 +4,33 @@ Created on Mon Oct 29 13:48:01 2018
 
 Provides all neccessary operational functions in the tinysoft system.
 
+Require Python 3 environment
+Require TSLPy3 package
+Need Accesable TinySoft accounts
+
+
 @author: szlsd
 """
+import TSLPy3 as ts
+import pandas as pd
+import os
+import datetime
+import time
 
 # 1. Login
+# Please config your account information loccally, first column "Account" sec column "Password"
+# TinySoft python application require local setups, please read LanguageGuide in the TS user console.
 
+
+acc_f = pd.read_csv("D:/TSaccount.csv")
+
+ts.ConnectServer("tsl.tinysoft.com.cn",443) 
+
+dl = ts.LoginServer(acc_f.Account[0], acc_f.Password[0]) #Tuple(ErrNo,ErrMsg) Login
+
+
+
+ts.Disconnect()  # Log out.
 
 # 2. GetData
 
