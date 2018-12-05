c41e00e32a864c7506f711e9e7fd5a854a13de12
SidGor <szlsd123@163.com>
2018-12-05 17:33:39 +0800

    adding tushare files and some data cleaning codes

--- DataImport/DataCleaning.py
+++ DataImport/DataCleaning.py
@@ -10,10 +10,22 @@ situation in China that stocks will be suspended from trading for days even year
 """
 
 import pandas as pd
+import numpy as np
+import datetime
 import GroupFactor
 
-raw_data = pd.read_csv('D:\PythonDir\winddata\AS_%Ret20040101_20181113_wkly.csv')
-IPO_table = pd.read_csv('D:\PythonDir\winddata\AS_IPOdate20040101_20181113_wkly.csv')
+raw_data = pd.read_csv('D:/PythonDir/winddata/AS_%Ret20040101_20181113_wkly.csv')
+o_file = open('D:/PythonDir/tusharedata/listing_StockInfo.csv')
+IPO_table = pd.read_csv(o_file, dtype = 'str')
 
 def RmvNewStocks(raw_data, IPO_table, silience_date = 30):
+    
+    IPO_table.list_date = IPO_table.list_date.apply(lambda x: datetime.datetime.strptime(x, '%Y%m%d'))
+    raw_data.Date = raw_data.Date.apply(lambda x: datetime.datetime.strptime(x, '%Y/%m/%d'))
+    
+    NotIn_list = list(set(raw_data.columns.drop('Date')) - set(IPO_table.ts_code))
+    
+    
+    
     pass
+
